f36655e20e6e49e65bf746b37055e97ac4d5b316
Razoyo <pbyrne@razoyo.com>
2024-06-15 09:35:00 -0500

    basic weather API is working

--- lib/grassFarm/weather_client.ex
+++ lib/grassFarm/weather_client.ex
@@ -1,20 +1,23 @@
 defmodule WeatherClient do
   use Tesla
 
-  # Specify the base URL of the weather API
-  @base_url "http://api.openweathermap.org/data/2.5/onecall"
+  # Specify the base URL of the weather API - NOTE this is only call I can make with free plan
+  @base_url "http://api.openweathermap.org/data/2.5/forecast"
+  @lat "33.06480"
+  @lon "96.88968"
+  @appid "09c2e314057e0ae4a34ff5a53711934e" # For testing ONLY TODO remove after it works!!!
 
   # Define your Tesla middleware stack
   plug Tesla.Middleware.BaseUrl, @base_url
   plug Tesla.Middleware.JSON
-  plug Tesla.Middleware.Query, [appid: "your_api_key", units: "metric"]
+  plug Tesla.Middleware.Query, [appid: @appid, units: "metric"]
   plug Tesla.Middleware.Headers, [{"User-Agent", "Tesla"}]
 
   # Function to fetch rainfall data for the past 24 hours
-  def get_rainfall(lat, lon) do
-    case get("/timemachine", query: [lat: lat, lon: lon, dt: :os.system_time(:second) - 86400]) do
+  def get_rainfall(lat \\ @lat, lon \\ @lon) do
+    case get("/", query: [lat: lat, lon: lon]) do
       {:ok, %Tesla.Env{status: 200, body: body}} ->
-        parse_rainfall(body)
+        body
 
       {:ok, %Tesla.Env{status: status}} ->
         {:error, "Request failed with status: #{status}"}
