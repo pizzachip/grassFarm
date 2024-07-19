55e4f23b3825cd5907669a76a3073b1deee31ffd
Paul Byrne <pbyrne@razoyo.com>
2024-07-19 09:32:33 -0500

    changed weather api to tomorrow.io

--- lib/grassFarm/weather_client.ex
+++ lib/grassFarm/weather_client.ex
@@ -1,39 +1,26 @@
-defmodule WeatherClient do
+defmodule WeatherClientTomorrow do
   use Tesla
 
-  # Specify the base URL of the weather API - NOTE this is only call I can make with free plan
-  @base_url "https://www.ncei.noaa.gov/cdo-web/api/v2/"
-  @datasetid "PRECIP_HLY"
-  @locationid "ZIP:75056"
-  @startdate "2024-06-14" 
-  @enddate "2024-06-15"
-  @token "jmClsFqQLIhfFIBVZzQxPfMgKiPjEeeM" # For testing ONLY TODO remove after it works!!!
+  # Specify the base URL of the weather API
+  @base_url "https://api.tomorrow.io/v4/weather"
 
   # Define your Tesla middleware stack
-  plug Tesla.Middleware.BaseUrl, @base_url
-  plug Tesla.Middleware.JSON
-  plug Tesla.Middleware.Query, [units: "metric"]
-  plug Tesla.Middleware.Headers, [
-    {"User-Agent", "Tesla"},
-    {"token", @token}
-  ]
+  plug(Tesla.Middleware.BaseUrl, @base_url)
+  plug(Tesla.Middleware.JSON)
 
-  # Function to fetch rainfall data for the past 24 hours
-  def get_weather(
-    locationid \\ @locationid,
-    datasetid \\ @datasetid,
-    locationid \\ @locationid,
-    startdate \\ @startdate,
-    enddate \\ @enddate
-  ) do
-    case get("/data", query: [
-      datasetid: datasetid,
-      locationid: locationid,
-      startdate: startdate,
-      enddate: enddate
-    ]) do
+  plug(Tesla.Middleware.Query,
+    apikey: "",
+    location: "33.090320,-96.914140",
+    timesteps: "1h",
+    units: "metric"
+  )
+
+  plug(Tesla.Middleware.Headers, [{"User-Agent", "Tesla"}, {"accept", "application/json"}])
+
+  def get_weather_history() do
+    case get("/history/recent") do
       {:ok, %Tesla.Env{status: 200, body: body}} ->
-        body
+        parse_weather(body)
 
       {:ok, %Tesla.Env{status: status}} ->
         {:error, "Request failed with status: #{status}"}
@@ -43,12 +30,27 @@ defmodule WeatherClient do
     end
   end
 
-#   # Helper function to parse the rainfall data from the API response
-#   defp parse_rainfall(%{"hourly" => hourly_data}) do
-#     hourly_data
-#     |> Enum.map(&(&1["rain"]["1h"] || 0))
-#     |> Enum.sum()
-#     |> (&{:ok, &1}).()
-#   end
-end
+  def get_weather_forecast() do
+    case get("/forecast") do
+      {:ok, %Tesla.Env{status: 200, body: body}} ->
+        parse_weather(body)
 
+      {:ok, %Tesla.Env{status: status}} ->
+        {:error, "Request failed with status: #{status}"}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp parse_weather(body) do
+    body["timelines"]["hourly"]
+    |> Enum.map(fn x ->
+      {
+        x["time"],
+        x["values"]["rainAccumulation"],
+        x["values"]["temperature"]
+      }
+    end)
+  end
+end
